5f68a9dea6f1f20dc88e6475168a461ede2fa0b8
jochen <jgb.btask@gmail.com>
2021-05-20 17:39:13 +0000

    Added vpc module2

--- vpc.tf
+++ vpc.tf
@@ -3,12 +3,12 @@ module "network" {
   version = "2.5.0"
   network_name = "hashicat-btask-vpc"
   project_id = var.project
-  
+
   subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+  ]
 }
-]
